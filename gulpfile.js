940776ff44e581f17fde9218334bfda6041635db
Moad, Karl <karl.moad@gmail.com>
2017-03-07 13:25:03 -0500

    gulp file addition

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/**
+ * @author Karl Moad <github.com/karlmoad>
+ */
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('lib/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+
+
